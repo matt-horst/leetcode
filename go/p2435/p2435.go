43dc29d0d1fc50b7be4baca3f0691ca6455cbd17
Matt Horst <mattahorst@gmail.com>
2025-11-25 17:39:57 -0700

    solves problem 2435 with alternatively with resursion

--- go/p2435/p2435.go
+++ go/p2435/p2435.go
@@ -1,6 +1,37 @@
 package p2435
 
+import "fmt"
+
 const MOD int = 1e9+7
+
+func solve(i, j, rem int, dp [][]map[int]int, grid [][]int, k int) int {
+	if i == 0 && j == 0 {
+		if (grid[0][0] + rem) % k == 0 {
+			return 1
+		} else {
+			return 0
+		}
+	}
+
+	if ans, ok := dp[i][j][rem]; ok {
+		return ans
+	}
+
+	total := 0
+	if i > 0 {
+		total += solve(i - 1, j, (grid[i][j] + rem) % k, dp, grid, k)
+		total %= MOD
+	}
+
+	if j > 0 {
+		total += solve(i, j - 1, (grid[i][j] + rem) % k, dp, grid, k)
+		total %= MOD
+	}
+
+	dp[i][j][rem] = total
+
+	return total
+}
 
 func numberOfPaths(grid [][]int, k int) int {
 	n := len(grid)
@@ -15,26 +46,9 @@ func numberOfPaths(grid [][]int, k int) int {
 		}
 	}
 
-	dp[0][0][grid[0][0] % k] = 1
-	for i := range n {
-		for j := range m {
-			if i > 0 {
-				for key, val := range dp[i-1][j] {
-					rem := (key + grid[i][j]) % k
-					dp[i][j][rem] += val
-					dp[i][j][rem] %= MOD
-				}
-			}
+	solve(n-1, m-1, 0, dp, grid, k)
 
-			if j > 0 {
-				for key, val := range dp[i][j-1] {
-					rem := (key + grid[i][j]) % k
-					dp[i][j][rem] += val
-					dp[i][j][rem] %= MOD
-				}
-			}
-		}
-	}
+	fmt.Printf("dp: %v\n", dp)
 
 	return dp[n-1][m-1][0]
 }
